5ea8e4f0702245d6955d60da15fce48b50a1bc5c
yuuuna-lee <yuunalee1050@gmail.com>
2025-03-16 00:52:50 +0900

    feat: set up the build process

--- application.py
+++ application.py
@@ -412,5 +412,6 @@ def analyze_text():
     )
 
 
+# 이 파일이 직접 실행될 때만 서버 실행
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    application.run(debug=False, host="0.0.0.0", port=5000)
